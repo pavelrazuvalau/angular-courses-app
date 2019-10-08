6bba950c9e03423febfe6043ab793f829528f8c3
Pavel Razuvalau <Pavel_Razuvalau@epam.com>
2019-10-08 15:42:51 +0300

    Modify json-server

--- json-server/services/core/courses/courses.middleware.js
+++ json-server/services/core/courses/courses.middleware.js
@@ -2,26 +2,37 @@ const express = require('express');
 const router = express.Router();
 const url = require('url');
 
-module.exports = (server) => {
+module.exports = server => {
+  router.get('/courses', (req, res) => {
+    // const token = req.headers['authorization'];
 
-	router.get('/courses', (req, res) => {
-		let url_parts = url.parse(req.originalUrl, true),
-			query = url_parts.query,
-			from = query.start || 0,
-			to = +query.start + +query.count,
-			courses = server.db.getState().courses;
-		
-			if (!!query.textFragment) {
-				courses = courses.filter((course) => course.name.concat(course.description).toUpperCase().indexOf(query.textFragment.toUpperCase()) >= 0);
-			}
+    // if (!token) {
+    //   return res.status(401).json({ message: 'Unauthorized' });
+    // }
 
-		if (courses.length < to || !to) {
-			to = courses.length;
-		}
-		courses = courses.slice(from, to);
-		
-		res.json(courses);
-	});
-	
-	return router;
+    let url_parts = url.parse(req.originalUrl, true),
+      query = url_parts.query,
+      from = query.start || 0,
+      to = +query.start + +query.count,
+      courses = server.db.getState().courses;
+
+    if (!!query.textFragment) {
+      courses = courses.filter(
+        course =>
+          course.name
+            .concat(course.description)
+            .toUpperCase()
+            .indexOf(query.textFragment.toUpperCase()) >= 0
+      );
+    }
+
+    if (courses.length < to || !to) {
+      to = courses.length;
+    }
+    courses = courses.slice(from, to);
+
+    res.json(courses);
+  });
+
+  return router;
 };
